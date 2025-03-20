74ee7c8200851c6f28bd6dfce1bdebd858f9276d
Brandon <BrandonPilane@gmail.com>
2025-03-21 00:18:14 +0200

    Playing with tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<body class="bg-black text-white">
+	<div class="flex h-screen flex-col items-center justify-center">
+		<div class="flex flex-col items-center justify-center">
+			<h1 class="text-6xl font-bold">Gif Thing</h1>
+			<p class="text-2xl">A gif generator for the web</p>
+		</div>
+	</div>
+</body>
